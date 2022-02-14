683f476a9157fee09da1b39f350e11fb436b1c1c
lminchala <73615264+lminchala@users.noreply.github.com>
2022-02-14 18:12:24 -0500

    Day 1 goals 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,14 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Question(props){
+return <p>{props.Quest}</p>
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  console.log(data[0].question.text);
+  let [Quests, setQuests]=useState(0);
+  return <div className="app">Trivia! <Question Quest={data[0].question.text}/> </div>;
 }
 
 export default App;
