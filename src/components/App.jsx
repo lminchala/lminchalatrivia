e9098b93c5df3f55da0f8bda0883e68780f706e7
lminchala <73615264+lminchala@users.noreply.github.com>
2022-03-07 18:29:27 -0500

    Goal 2 1/2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,23 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Answer(props){
+ return <ul>{props.Answer}</ul>
+}
+////ans.map((choice)=><p>{choice}</p>) mira 121122
+
 function Question(props){
-return <p>{props.Quest}</p>
+return(<div><p>{props.Quest}</p>
+<Answer Answer={props.Answer}/></div>)
+
 }
 function NextQuestion(props){
 return<button>button</button>
 }
 function App() {
-  console.log(data[0].question.text);
+  console.log(data[0].question.choices);
   let [Quests, setQuests]=useState(0);
-  return <div className="app">Trivia! <Question Quest={data[0].question.text}/><NextQuestion/> </div>;
+  return <div className="app">Trivia! <Question Quest={data[0].question.text} Answer={data[0].question.choices}/> <NextQuestion /> </div>;
 }
 
 export default App;
