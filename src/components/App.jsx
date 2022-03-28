32bf41e8a18e8799510f1331374e600c4b984654
lminchala <73615264+lminchala@users.noreply.github.com>
2022-03-28 18:20:00 -0400

    day 2 goal 2 complete.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import data from "../sample_data.json";
 
 function Answer(props) {
   let newItems = props.Answer.map(displayAnswers);
-  
+
   function displayAnswers(answerChoices) {
     return <p>{answerChoices}</p>;
   }
@@ -23,24 +23,30 @@ function NextQuestion(props) {
   return <button>Next Question</button>;
 }
 function App() {
+
+  let[currentQuestNum, setcurrentQuestionNum]= useState(0);
   let [answerDisplay, setAnswerDisplay] = useState("");
+  function change(props){
+    return setcurrentQuestionNum(1);
+  }
   //console.log(data[0].question.choices);
   let [Quests, setQuests] = useState(0);
+  let question = data[0].question;
   return (
     <div className="app">
       Trivia!{" "}
       <Question
-        Quest={data[0].question.text}
-        Answer={data[0].question.choices}
+        Quest={question.text}
+        Answer={question.choices}
       />
-      <NextQuestion /> 
-      <button onClick={()=>{
-setAnswerDisplay(data[0].question.choices.[data[0].question.choices.correct_choice_index]);
-
-      }}
-      
-      >The correct answer is {answerDisplay}</button>
-      
+      <NextQuestion nextNum={props.}/>
+      <button
+        onClick={() => {
+          setAnswerDisplay(question.choices[question.correct_choice_index]);
+        }}
+      >
+        The correct answer is {answerDisplay}
+      </button>
     </div>
   );
 }
